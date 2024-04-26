c064e5a29f6bbf308071ea2079c15333b182896f
Richard Schulze <50849775+richardschulze@users.noreply.github.com>
2024-04-26 11:01:03 +0200

    Prepare v0.0.2 (#6)

--- PyPI/test_publish.sh
+++ PyPI/test_publish.sh
@@ -4,4 +4,4 @@ if [ -d "./dist" ] && [ -n "$(ls -A "./dist")" ]; then
   echo "dist folder exists and is not empty"
   exit
 fi
-hatch build && hatch publish -r test
+hatch build && hatch publish --user __token__ -r test
